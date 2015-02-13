54d2b5bd54947de193279770f1f86f92f2742718
Mark Tonkinson <DHTMaster@gmail.com>
2015-02-13 09:43:02 -0700

    Removed many of the replace statements with a global regex replace. Having issues removing special quote characters. Need to start thinking about how to visualize and how to remove when ready to start another analysis

--- analyzer.js
+++ analyzer.js
@@ -2,15 +2,18 @@ var analyze = function(){
 	var text = document.getElementById('text-area').value;
 	var wordStore = {};
 	var sortedWords = [];
-
+	text = text.replace(/(\r\n|\n|\r)/gm," ")
+	//this would replace all the apstrophes etc.
+	//text = text.replace(/(\W)/gm, ' ')
+	text = text.replace(/(,|[.]|;|:|!|\[|\]|\)|\(|\{|\}|\?|")/gm,'')
 	text = text.split(' ')
 
 	console.log('text:', text)
 	for(var i=0; i<text.length; i++){
 		var word = text[i]
 		word = word.toLowerCase().trim();
-		word = word.replace(',','').replace('.', '').replace(';', '').replace('(','').replace(')','').replace(':','')
-		if(word ===""){
+		word = word.replace('&#8220','').replace('&#8221', '').replace('"','').replace('&#8212','')
+		if(word === ""){
 
 			continue;
 		}
@@ -23,7 +26,7 @@ var analyze = function(){
 			}
 		}
 	}
-
+	console.log(wordStore)
 	for(var prop in wordStore){
 		sortedWords.push(wordStore[prop])
 	};
@@ -31,9 +34,12 @@ var analyze = function(){
 	sortedWords.sort(function(a, b){
 		return b.counter-a.counter;
 	})
-	for(var j=0; j<=sortedWords.length; j++){
+	for(var j=0; j<sortedWords.length; j++){
 		var results = document.getElementById('results')
 		var newP = document.createElement('p')
+		if(!sortedWords[j].text){
+			console.log(sortedWords[j])
+		}
 		var newText = document.createTextNode(sortedWords[j].text + ': ' + sortedWords[j].counter)
 		newP.appendChild(newText);
 		results.appendChild(newP)
