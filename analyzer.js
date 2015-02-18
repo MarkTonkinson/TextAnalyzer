98275a6ee79eed4fa75f93b650b7e8b51d79991d
Mark Tonkinson <DHTMaster@gmail.com>
2015-02-18 13:00:22 -0700

    Playing around with regexs

--- analyzer.js
+++ analyzer.js
@@ -1,13 +1,52 @@
+var simplify = {
+	removeCommons: function(str){
+		str = str.replace(/(?=for\b)\w+/gm,'')
+		str = str.replace(/(?=the\b)\w+/gm,'')
+		str = str.replace(/(?=or\b)\w+/gm,'')
+		str = str.replace(/(?=and\b)\w+/gm,'')
+
+		str = str.replace(/(?=to\b)\w+/gm,'')
+		str = str.replace(/(?=from\b)\w+/gm,'')
+		str = str.replace(/(?=a\b)\w+/gm,'')
+		str = str.replace(/(?=on\b)\w+/gm,'')
+		str = str.replace(/(?=which\b)\w+/gm,'')
+		str = str.replace(/(?=there\b)\w+/gm,'')
+		str = str.replace(/(?=he\b)\w+/gm,'')
+		str = str.replace(/(?=him\b)\w+/gm,'')
+		str = str.replace(/(?=she\b)\w+/gm,'')
+		str = str.replace(/(?=her\b)\w+/gm,'')
+		str = str.replace(/(?=they\b)\w+/gm,'')
+		str = str.replace(/(?=them\b)\w+/gm,'')
+		str = str.replace(/(?=we\b)\w+/gm,'')
+
+
+		console.log(str)
+
+		return str;
+	}
+}
+
+var removeText = function(){
+	var text = document.getElementById('text-area').value;
+	
+	var doThis = simplify.removeCommons(text);
+	text = doThis
+}
 var analyze = function(){
 	var text = document.getElementById('text-area').value;
 	var wordStore = {};
 	var sortedWords = [];
+	//this removes new line characters
 	text = text.replace(/(\r\n|\n|\r)/gm," ")
 	//this would replace all the apstrophes etc.
 	//text = text.replace(/(\W)/gm, ' ')
 	text = text.replace(/(,|[.]|;|:|!|\[|\]|\)|\(|\{|\}|\?|")/gm,'')
-	text = text.split(' ')
+	
 
+	
+
+
+	text = text.split(' ')
 	console.log('text:', text)
 	for(var i=0; i<text.length; i++){
 		var word = text[i]
@@ -35,13 +74,15 @@ var analyze = function(){
 		return b.counter-a.counter;
 	})
 	for(var j=0; j<sortedWords.length; j++){
+		
+		
 		var results = document.getElementById('results')
 		var newP = document.createElement('p')
-		if(!sortedWords[j].text){
-			console.log(sortedWords[j])
-		}
 		var newText = document.createTextNode(sortedWords[j].text + ': ' + sortedWords[j].counter)
+
 		newP.appendChild(newText);
 		results.appendChild(newP)
+		
+
 	}
 }
